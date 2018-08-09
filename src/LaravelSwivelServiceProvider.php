b60dbbbeefee1ac35d5557eac105eb69ba27cd1b
Pablo Molina <web.kod3r@gmail.com>
2018-08-09 12:02:16 -0400

    Using the new configuration file

--- src/LaravelSwivelServiceProvider.php
+++ src/LaravelSwivelServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace Webkod3r\LaravelSwivel;
 
-use Illuminate\Contracts\Container\Container;
+use \Illuminate\Contracts\Container\Container;
 use \Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\Application as LaravelApplication;
-use Laravel\Lumen\Application as LumenApplication;
+use \Illuminate\Foundation\Application as LaravelApplication;
+use \Laravel\Lumen\Application as LumenApplication;
 
 class LaravelSwivelServiceProvider extends ServiceProvider {
 
@@ -14,7 +14,7 @@ class LaravelSwivelServiceProvider extends ServiceProvider {
      *
      * @var string
      */
-    const VERSION = '0.2.1';
+    const VERSION = '0.2.2';
 
     /**
      * Indicates if loading of the provider is deferred.
@@ -29,24 +29,36 @@ class LaravelSwivelServiceProvider extends ServiceProvider {
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
 
+    private function configPath() {
+        return realpath($raw = __DIR__ . '/../config/swivel.php') ?: $raw;
+    }
+
     /**
      * Setup the config.
      *
      * @param \Illuminate\Contracts\Container\Container $app
-     *
      * @return void
      */
-    protected function setupConfig(Container $app)
-    {
-        $source = realpath($raw = __DIR__ . '/../config/laravel-swivel.php') ?: $raw;
+    protected function setupConfig(Container $app) {
+        $source = $this->configPath();
 
         if ($app instanceof LaravelApplication && $app->runningInConsole()) {
-            $this->publishes([$source => config_path('laravel-swivel.php')]);
+            $this->publishes([$source => config_path('swivel.php')]);
         } elseif ($app instanceof LumenApplication) {
-            $app->configure('laravel-swivel.php');
+            $app->configure('swivel');
         }
 
-        $this->mergeConfigFrom($source, 'laravel-swivel');
+        // merge specific configurations
+        $source = $app->getConfigurationPath('swivel') ?: $source;
+        $this->mergeConfigFrom($source, 'swivel');
+    }
+
+    /**
+     * Publish swivel assets.
+     */
+    private function publishAssets() {
+        app()->make('config')->set('swivel', app()->getConfigurationPath('swivel'));
+        app()->configure('swivel');
     }
 
     /**
